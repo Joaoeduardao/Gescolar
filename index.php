4af868b1be511236972efc3018f9f6f61d0e0ef7
Aluno <aluno@etec.sp.gov.br>
2019-11-25 12:38:49 -0200

    Pasta Gescolar atualizada

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<?php
+
+// caso o usário clique em sair 
+if(isset($_REQUEST['sair'])) {
+
+    unset($SESSION['gescolar_dados_usuario']) // destroi a sessão de autenticação do usuário.
+    header("location login.php"); // redireciona para a página de login 
+}
+
+// protegendo a página contra acesso sem autenticação
+if(!isset($SESSION['gescolar_dados_usuario'])) {
+    header("location.php"); // redireciona para o login.
+}
+
+// abreviando o nome da variável que contém os dados do usuário.
+$usuario = $_SESSION['gescolar_dados_usuario'];
+
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,13 +23,12 @@
     </head>
     <body>
         <div id="global">
-            <h1>GESCOLAR</h1>
-            <nav>
-                <ul>
-                     <li><a href="cadastro_alunos.php">Cadastrar Aluno</a></li>
-                     <li><a href="lista_alunos.php">Lista de Alunos </a></li>
-                </ul>
-            </nav>
+
+            <!-- exibindo o nome do usuario que esta guardado na sessao, com os outros dados -->
+            <h1>GESCOLAR <small>, bem-vindo <?= $usuario['nome'] ?> </small> </h1>
+           
+           <?php include_once 'includes/cabecalho.php' ?>
+           
         </div>
     </body>
 </html>
